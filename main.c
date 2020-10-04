36b42a508c433357c442834daf229ceeb2194d2e
root <root@ip-172-31-11-213.ap-northeast-2.compute.internal>
2020-10-04 02:12:45 +0000

    wrapper functions 작성 완료 및 1
    wrapper functions 작성
    소켓용, 에러용 버퍼사이즈 MAXLINE, MAXLINE_E 로 구분

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <stdio.h>		// fputs()
 #include <stdlib.h>		// exit()
 
+#define MAXLINE 1024
+
 /* headers for error handling */
 #include <stdarg.h>	// varying arguments (va_~~~), vsnprintf()
 #include <errno.h>	// errno
@@ -16,10 +18,12 @@
 #include <string.h>	// strlen(), strerror(), strcat()
 #include <syslog.h>	// syslog(), LOG_ERR
 
-#define MAXLINE 100
+#define MAXLINE_E 100
 
 
-/* for error handling */
+/**
+ * for error handling 
+ */
 int daemon_proc;	// set nonzero by daemon_init(), only works when the process is daemonized
 
 static void err_doit(int, int, const char*, va_list);
@@ -58,18 +62,18 @@ void err_quit(const char *fmt, ...){
 static void err_doit(int errnoflag, int level, const char* fmt, va_list ap){
 
 	int errno_save, n;
-	char buf[MAXLINE + 1];
+	char buf[MAXLINE_E + 1];
 
 	// errno: integer variable, set by system calls and some library functions on error
 	// to use, errno should be saved to prevent from being changed by other calls
 	errno_save = errno;
 
-	vsnprintf(buf, MAXLINE, fmt, ap);	// vsnprintf() invokes va_arg macro
+	vsnprintf(buf, MAXLINE_E, fmt, ap);	// vsnprintf() invokes va_arg macro
 	// could use vsprintf when vsnprintf doesn't work, but not safe
 
 	n = strlen(buf);
 	if (errnoflag)
-		snprintf(buf+n, MAXLINE-n, ": %s", strerror(errno_save));	// strerror() returns the string describes the errno_save
+		snprintf(buf+n, MAXLINE_E-n, ": %s", strerror(errno_save));	// strerror() returns the string describes the errno_save
 	strcat(buf, "\n");
 
 	if (daemon_proc){	// only works when the process is daemonized
@@ -81,6 +85,59 @@ static void err_doit(int errnoflag, int level, const char* fmt, va_list ap){
 	}
 	
 	return;
+}
+
+
+
+
+/**
+ * wrapper functions
+ * Socket, Connect, Read, Fputs, Inet_pton
+ */
+int Socket(int domain, int type, int protocol){
+	
+	int n;
+	if ( (n = socket(domain, type, protocol)) < 0 )
+		err_sys("socket error");
+
+	return n;	// sockfd integer number on success
+}
+
+int Connect(int sockfd, struct sockaddr *addr, socklen_t addrlen){
+
+	int n;
+	if ( (n = connect(sockfd, addr, addrlen)) < 0 )
+		err_sys("connect error");
+
+	return n;	// 0 on success
+}
+
+ssize_t Read(int fd, void* buf, size_t count){
+
+	int n;
+	if ( (n = read(fd, buf, count)) < 0 )
+		err_sys("read error");
+
+	return n;	// the number of bytes read on success
+}
+
+int Fputs(const char* s, FILE* stream){
+
+	int n;
+	if ( (n = fputs(s, stream)) == EOF )
+		err_sys("fputs error");
+
+	return n;	// nonnegative number on success
+}
+
+int Inet_pton(int af, const char* src, void* dst){
+
+	int n;
+	// inet_pton() returns 0 or -1 when address or address family is invalid
+	if ( (n = inet_pton(af, src, dst)) <= 0 )
+		err_quit("inet_pton error for %s", src);
+
+	return n;	// 1 on success
 }
 
 
@@ -99,10 +156,8 @@ int main(int argc, char **argv){
 	// AF_INET : communication domian for IPv4 Internet Protocols
 	// SOCK_STREAM : communication type for TCP Connections
 	// 0 : protocol setting. Only single protocol exists for tcp stream sockets, so can use 0 instead of IPPROTO_TCP.
-	// socket() returns -1 on error
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
-		err_sys("socket error");
-
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	
 	bzero(&servaddr, sizeof(servaddr));
 	
 	// all address/port should be manipulated to  network byte order
@@ -110,20 +165,16 @@ int main(int argc, char **argv){
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(13);	// 13 is reserved port for daytime protocol
 	
-	// inet_pton() returns 0 or -1 when address or address family is invalid
-	if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0 )
-		err_quit("inet_pton error for %s", argv[1]);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	// sockaddr structure only used for pointer casting
-	// connect() returns 0 on success, -1 on error
-	if ( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0 )
-		err_sys("connect error");
+	Connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
 	// read() returns -1 on error, the number of bytes on success
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0 ){
+	// Read() checks for error by itself. it is double checking
+	while ( (n = Read(sockfd, recvline, MAXLINE)) > 0 ){
 		recvline[n] = 0;	// set null terminate
-		if ( fputs(recvline, stdout) == EOF )
-			err_sys("fputs error");
+		Fputs(recvline, stdout);
 	}
 	if (n<0)
 		err_sys("read error");
